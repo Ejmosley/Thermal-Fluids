b6ea379c9a8fdfd563ce463c48d0d5f90ad2fa5e
Ejmosley <ejmosley@umich.edu>
2016-12-07 20:30:26 -0500

    Add files via upload

--- Finite Difference Method of an Annular Fin with Rectangular Profile.py
+++ Finite Difference Method of an Annular Fin with Rectangular Profile.py
@@ -1,4 +1,4 @@
-︠acd9e553-5e81-4017-b116-5390f1fc77cb︠
+
 #FDM1
 #import numpy
 #from matplotlib import pyplot
@@ -93,11 +93,6 @@ for i in xrange(1,N-1):
 #print solution
 
 #xn=[0;xn]; Tn=[T1;Tn];
-︡58c92208-62eb-44da-9811-676bbca303eb︡{"html":"<div align='center'>Fin efficiency is: 6.340996%</div>"}︡{"html":"<div align='center'>Fin effectiveness is: 273.45544</div>"}︡{"raw_input":{"prompt":"Enter the number of unknowns (N) for problem:"}}︡{"delete_last":true}︡{"raw_input":{"prompt":"Enter the number of unknowns (N) for problem:","submitted":true,"value":"5"}}︡{"stdout":"array([ 0.074,  0.074,  0.37 ])\n"}︡{"stderr":"Error in lines 40-46\nTraceback (most recent call last):\n  File \"/projects/sage/sage-7.3/local/lib/python2.7/site-packages/smc_sagews/sage_server.py\", line 968, in execute\n    exec compile(block+'\\n', '', 'single') in namespace, locals\n  File \"<string>\", line 3\n    [A(i,i-Integer(1))=Integer(1)-((dr)/(Integer(2)*rn)),\n                      ^\nSyntaxError: invalid syntax\n"}︡{"done":true}︡
-︠615036a0-0598-4432-b098-b44700743bc0︠
-
-
-
 
 
 
